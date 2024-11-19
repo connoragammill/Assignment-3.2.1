55095d6624f8d41e8b2b454d76218433056147c8
connor gammill <connor.gammill@outlook.com>
2024-11-19 14:17:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment_3._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                { 2, 3, 4 },
+                { 1, 4, 6 }
+            };
+            ShowMatrix(matrix);
+        }
+
+        static void ShowMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0;  j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("| " + matrix[i, j] + " ");
+                }
+                Console.WriteLine("|");//Console.WriteLine to add the "|" in betwwen the numbers
+            }
+        }
+    }
+}
